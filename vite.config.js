f9c3cbcfefbbbfe58ce0e741df05caed9afd32ba
Braxton Hotton <bhotton25@gmail.com>
2024-07-13 15:21:33 -0600

    Now trying to fix .env issue

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
 import { resolve } from "path";
 // eslint-disable-next-line import/namespace
 import { defineConfig } from "vite";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
   root: "src/",
-
   build: {
     outDir: "../dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html")
       },
+    },
+  },
+  define: {
+    "import.meta.env": {
+      VITE_OPENWEATHER_KEY: process.env.VITE_OPENWEATHER_KEY,
     },
   },
 });
